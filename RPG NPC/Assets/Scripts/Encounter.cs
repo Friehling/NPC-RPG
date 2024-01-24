07e151c9bd56ec7c823438fc3849745b90db0a88
sebEFN <94054113+sebEFN@users.noreply.github.com>
2024-01-24 11:42:09 +0100

    made pokémon grass

--- RPG NPC/Assets/Scripts/Encounter.cs
+++ RPG NPC/Assets/Scripts/Encounter.cs
@@ -1,14 +1,19 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Jobs;
 using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 public class Encounter : MonoBehaviour
 {
     public bool switchScene = false;
     public int index;
-    public GameObject player;    
+    public GameObject player;
+    public int randomNumber;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -28,8 +33,13 @@ public class Encounter : MonoBehaviour
     {
         if (collider.gameObject.transform)
         {
+           randomNumber = Random.Range(0, 100);
+            if (randomNumber == 9)
+            {
+              SceneManager.LoadScene(index);
+            }
             Debug.Log("hit");
-            //SceneManager.LoadScene(index);
+            
            
         }
     }
