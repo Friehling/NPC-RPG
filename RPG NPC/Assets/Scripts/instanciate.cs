c1c21aaf76b451f5b6a0c24bdff326b0adb18db4
sebEFN <sebastian.efnielsen@gmail.com>
2024-02-19 13:38:54 +0100

    player spawn

--- RPG NPC/Assets/Scripts/instanciate.cs
+++ RPG NPC/Assets/Scripts/instanciate.cs
@@ -12,7 +12,7 @@ public class instanciate : MonoBehaviour
     {
        //Control = GetComponent<control>();
        Control = GameObject.Find("controller").GetComponent<control>();
-        Instantiate(prefab[Control.rember], new Vector3(), Quaternion.identity);
+        Instantiate(prefab[Control.rember], new Vector2(-9,12), Quaternion.identity);
 
         Debug.Log("");
     }
